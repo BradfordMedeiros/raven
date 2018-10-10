45e4e4747f4c0eefc6a2f163953689a96d2b0155
brad medeiros <bradmedeiros0@gmail.com>
2018-10-09 22:31:28 -0700

    -

--- execute/util/algorithms/config_requirements/bayes.py
+++ execute/util/algorithms/config_requirements/bayes.py
@@ -1,55 +1,24 @@
 #  config file looks like
 #
-# person,gender,,feeling
-# doctor,male,,happy
-# doctor,female,,happy
-# engineer,male,,sad
-# dentist,male,,sad
-#
-#  ui:
-#  features: list of numbers
-#  predictors: list of numbers
-#  how to enforce? 
+#person,gender,,feeling
+#doctor,male,,happy
+#doctor,female,,happy
+#engineer,male,,sad
+#dentist,male,,sad
+from .common.is_valid_map import is_valid_map
 
 requirements = {
-	'features': 'string',
 	'predictors': 'string',
-	'num_features_min': 1,
-	'num_features_max': None,
+	'features': 'string',
 	'num_predictors_min': 1,
-	'num_predictors_max': 1
+	'num_predictors_max': None,
+	'num_features_min': 1,
+	'num_features_max': 1,
 }
 
 def is_valid_data_map(data_map):
-	num_features = 0
-	num_predictors = 0
+	return is_valid_map(data_map, requirements)
 
-	if requirements['num_features_max'] != None and num_features > requirements['num_features_max']:
-		return False
-	if requirements['num_features_min'] != None and num_features < requirements['num_features_min']:
-		return False
-
-	if requirements['num_predictors_max'] != None and num_predictors > requirements['num_predictors_max']:
-		return False
-	if requirements['num_predictors_min'] != None and num_predictors < requirements['num_predictors_min']:
-		return False
-
-	return True
-
-"""
-{
-	   person: {
-		  predictor: False,
-		  values: ['doctor','doctor','engineer', 'dentist']
-	   },
-	   gender: {
-		  predictor: False,
-		  values: ['male','female','male','male']
-	   },
-	   feeling: {
-		  predictor: True,
-		  values: ['happy','happy','sad','sad']
-	   }
-	}
-"""
+def get_requirements():
+	return requirements
 
