7fd61af0957839cd872735dd5b4921bb7bf33cc8
brad medeiros <bradmedeiros0@gmail.com>
2018-10-09 22:29:15 -0700

    get rid of excessive printing, output json

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 import json
 from execute.util.config.read_data_map import read_data_map
+from execute.execute_knearest import execute_knearest
 from execute.util.algorithms.arg_parsing.knearest import parse_args as parse_k_args
-from execute.util.algorithms.arg_parsing.bayes import parse_args as parse_bayes_args
-from execute.util.algorithms.implementation.testAlgorithm import main as testAlgorithm
 from execute.util.algorithms.config_requirements.knearest import is_valid_data_map as is_valid_data_map_knearest
 from execute.util.algorithms.config_requirements.knearest import get_requirements as get_requirements_knearest
-from execute.execute_knearest import execute_knearest
+from execute.util.algorithms.arg_parsing.bayes import parse_args as parse_bayes_args
+from execute.util.algorithms.implementation.testAlgorithm import main as testAlgorithm
+
 
 moduleName = sys.argv[1]
 modules = {
@@ -27,7 +28,6 @@ if moduleName == 'help' or moduleName == '-h':
 	print('knearest, bayes')
 	exit(0)
 
-
 module = modules[moduleName]
 module_args = sys.argv[2:]
 options = module['parse_args'](module_args)
@@ -39,58 +39,10 @@ if options.usage:
 
 data_map = read_data_map(options.data)
 valid_map = module['valid_map'](data_map)
+
 if not valid_map:
 	print('invalid config for knearest')
 	exit(1)
 
 result = module['execute'](data_map, json.loads(options.value))
 print json.dumps(result)
-
-
-"""
-def create_from_data_map(data_map):
-	data = {
-		'labels': data_map.keys()
-	}
-
-	predictors = [ ]
-	
-        all_values = []
-        for label in data_map:
-            values = data_map[label]['values']
-            all_values.append(values)
-
-        print ('| ', all_values)
-
-	return data
-
-k_form = create_from_data_map(data_map)
-print('k-form: ', k_form)
-"""
-
-
-#l = ['apple', 'go', 'bana']
-#predictors=[
-#	[ 0, 10, 24 ],
-#	[ 10, 10, 10]
-#] 
-#configData=[
-#	[ 20, 30, 30],
-#	[ 10, 11, 12]
-#]
-
-#{
-#	temperature: {
-#		values: [ 30, 23, 23, 34],
-#		is_predictor: True,
-#	}
-#	humidity: [
-#		values: [ 30, 23, 23, 34],
-#		is_predictor: False
-#	]
-#	///
-#}
-
-#knearest = algorithm(labels = l, predictors = predictors, configData = configData)
-#value = knearest.predict([[ 34, 41, 34 ]])
-#print('value: ', value)
